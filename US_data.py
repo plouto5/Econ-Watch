33c2218f1a20d12e6b0fc0a096328d865f194910
plouto5 <ploutos66p@gmail.com>
2018-06-01 01:29:49 -0500

    added data file US_data cleaning up code

---
+++ US_data.py
@@ -0,0 +1,13 @@
+
+# Quandl US Econ data API Code
+US_data_links = [       'FRED/GDP',
+                        'FRED/GDPC1',
+                        'FRED/CPIAUCSL',
+                        'FRED/BASE',
+                        'FRED/DFF',
+                        'FRED/UNRATE',
+                        'FRED/GFDEBTN',
+                        'FRED/PSAVERT',
+                        'FRED/MEHOINUSA672N',
+                        'FRED/NAPM'
+           ]
--- fetch.py
+++ fetch.py
@@ -1,24 +1,15 @@
+import os
 import quandl
 import time
+from directory import path
 from key import key
+from US_data import *
 
-# Quandl US Econ data API Code
-US_data_links = [            'FRED/GDP',
-                        'FRED/GDPC1',
-                        'FRED/CPIAUCSL',
-                        'FRED/BASE',
-                        'FRED/DFF',
-                        'FRED/UNRATE',
-                        'FRED/GFDEBTN',
-                        'FRED/PSAVERT',
-                        'FRED/MEHOINUSA672N',
-                        'FRED/NAPM'
-           ]
 
 quandl.ApiConfig.api_key = key
 
-for x in fx_links:
+for x in US_data_links:
     print(x)
     df = quandl.get(x)
-    df.to_csv(x[5:]+'.csv')
+    df.to_csv(os.path.join(path,x[5:]+'.csv'))
     time.sleep(20)
