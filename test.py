f0ed2e6d6442ee2a2585c4de76b91963f0f71069
Plouto5 <ploutos66p@gmail.com>
2018-06-13 17:57:57 -0700

    creating curses display

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
-import time
+import curses
 import csv
 
 
 
 """print variables in desired display"""
 
+# Start Curses
+stdscr = curses.initscr()
+curses.noecho()
+curses.cbreak()
+stdscr.keypad(True)
 
+
+# End Curses
+curses.nocbreak()
+stdscr.keypad(False)
+curses.echo()
+curses.endwin()
 
 
 """ Load variable's from csv """
