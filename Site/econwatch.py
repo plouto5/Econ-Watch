a3d1389094b3f9e1cac5a076da7bd242fbe39b06
Plouto5 <ploutos66p@gmail.com>
2018-06-21 04:14:52 +0000

    set variables in template

--- Site/econwatch.py
+++ Site/econwatch.py
@@ -1,21 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
-
-
-
-
-
+test = "Hello World!!"
 
 @app.route("/")
 def home():
-    return render_template('home.html')
-
-
-
-
-
+    return render_template('home.html', test=test)
 
 
 if __name__=='__main__':
