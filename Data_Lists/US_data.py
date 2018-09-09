1932c6214502cc35c099241f8477522636558b35
Plouto5 <ploutos66p@gmail.com>
2018-09-08 20:08:54 -0500

    added descriptions to data pulled from quandl

--- Data_Lists/US_data.py
+++ Data_Lists/US_data.py
@@ -1,15 +1,26 @@
 
 # Quandl US Econ data API Code
 US_data_links = [
+                        # Gross Domestic Product 
                         'FRED/GDP',
+                        # Real Gross Domestic Product
                         'FRED/GDPC1',
+                        # Consumer Price Index for All Urban Consumers
                         'FRED/CPIAUCSL',
+                        # St. Louis Adjusted Monetary Base
                         'FRED/BASE',
+                        # Effective Federal Funds Rate
                         'FRED/DFF',
+                        # Civilian Unemployment Rate
                         'FRED/UNRATE',
+                        # Federal Debt: Total Public Debt
                         'FRED/GFDEBTN',
+                        # Personal Saving Rate
                         'FRED/PSAVERT',
+                        # Real Median Household Income in US
                         'FRED/MEHOINUSA672N',
+                        # ISM Manufacturing: PMI Composite Index
                         'FRED/NAPM',
+                        # All Employees: Total Nonfarm Payrolls
                         'FRED/PAYNSA'
                 ]
