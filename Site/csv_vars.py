ddd0702af1f72994128e07f12238e9ac97f9fff6
Plouto5 <ploutos66p@gmail.com>
2018-09-10 22:49:55 -0500

    Still trying to display last row in csv file

--- Site/csv_vars.py
+++ Site/csv_vars.py
@@ -1,12 +1,25 @@
 import csv
 
-def csv_line1(csv):
+
+def lastrow(file):
     
-    with open(csv, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for line in csv_reader:
-            return line[1]
+    with open(file, newline='') as f:
+        row_count = sum(1 for row in f)
+        return row_count
+
+def csv_line1(file):
+    
+    #last = lastrow(file)
+    #print(last)
+    with open(file) as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            return row
 
 
-
+#    with open(file, newline='') as f:
+#      reader = csv.reader(f)
+#      row1 = next(reader)
+#      row2 = next(reader)
+#      return row2
 
--- Site/econwatch.py
+++ Site/econwatch.py
@@ -1,11 +1,19 @@
+import os
+from csv_vars import *
 from flask import Flask, render_template
+
+
+US_data_dir = "/home/cane/Documents/dev/Econ-Watch/Data/US"
+
 app = Flask(__name__)
 
 test = "Hello World!!"
 
+data = csv_line1(os.path.join(US_data_dir,'BASE.csv'))
+
 @app.route("/")
 def home():
-    return render_template('home.html', test=test)
+    return render_template('home.html', test=test, data=data)
 
 
 if __name__=='__main__':
